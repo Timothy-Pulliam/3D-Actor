9eef708341b9ba9e9d74308327cb59cdd2ccb3aa
Timothy Pulliam <timothy.dpulliam@gmail.com>
2020-09-06 19:04:10 -0400

    Generalized Physics Monitor GUI to work for any parent object / attribute combination. Accepts attributes in a list

--- PhysicsMonitor.gd
+++ PhysicsMonitor.gd
@@ -1,17 +1,45 @@
 extends Panel
 
+# attributes of the parent or specified children of the parent
+var attributes = ["acceleration", "velocity", "direction", "rotation_speed", 
+	"transform.basis", "transform.origin",
+	"Jump/jump_velocity", "Walk/walk_acceleration", "Walk/friction", "Walk/max_walk_speed",]
 
-var node = get_parent()
+func _ready():
+	for attribute in attributes:
+		var label = Label.new()
+		# We immediately have the attribute
+		if attribute.count("/") > 0:
+			# Seperate location node and attribute name
+			var i = attribute.find_last("/")
+			var attribute_node = attribute.substr(0, i)
+			attribute = attribute.substr(i+1)
+		
+		# set node name of label
+		if attribute.count(".") > 0:
+			# Node names can't contain "."
+			label.name = str(attribute.replace(".", "_"))
+		else:
+			label.name = attribute
+		label.size_flags_horizontal = SIZE_EXPAND_FILL
+		$VBoxContainer.add_child(label, true)
+
 
 func _physics_process(delta):
-	print("hello?")
-	$VBoxContainer/Label.text = "acceleration " + str(get_parent().acceleration)
-	$VBoxContainer/Label2.text = "velocity " + str(get_parent().velocity)
-	$VBoxContainer/Label3.text = "origin " + str(get_parent().transform.origin)
-	$VBoxContainer/Label4.text = "basis " + str(get_parent().transform.basis)
-	$VBoxContainer/Label5.text = "direction " + str(get_parent().direction)
-	$VBoxContainer/Label6.text = "jump_velocity " + str(get_parent().get_node("Jump").jump_velocity)
-	$VBoxContainer/Label6.text = "max_walk_speed " + str(get_parent().get_node("Walk").max_walk_speed)
-	$VBoxContainer/Label7.text = "walk_acceleration " + str(get_parent().get_node("Walk").walk_acceleration)
-	$VBoxContainer/Label8.text = "friction " + str(get_parent().get_node("Walk").friction)
-	
+	for attribute in attributes:
+		# if a node is provided (Node/attribute)
+		if attribute.count("/") > 0:
+			var i = attribute.find_last("/")
+			var attribute_node = attribute.substr(0, i)
+			attribute = attribute.substr(i+1)
+			# i.e. attribute is transform.origin
+			if attribute.count(".") > 0:
+				get_node("VBoxContainer/" + attribute.replace(".","_")).set_text(attribute + ": " + str(get_parent().get_node(attribute_node).get(attribute.split(".")[0])[attribute.split(".")[1]]))
+			else:
+				get_node("VBoxContainer/" + attribute).set_text(attribute + ": " + str(get_parent().get_node(attribute_node).get(attribute)))
+		else: 
+			if attribute.count(".") > 0:
+				get_node("VBoxContainer/" + attribute.replace(".","_")).set_text(attribute + ": " + str(get_parent().get(attribute.split(".")[0])[attribute.split(".")[1]]))
+			else:
+				get_node("VBoxContainer/" + attribute).set_text(attribute + ": " + str(get_parent().get(attribute)))
+			
